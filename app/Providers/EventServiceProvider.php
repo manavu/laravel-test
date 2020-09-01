cbd16b99f82294079fbd10c93643be8cabc44e43
Manabu Yoshida <manavu666@gmail.com>
2020-09-01 11:32:35 +0900

    feat: クエリがログファイルに出力されるようになります

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Database\Events\QueryExecuted;
+
+use Log;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -29,6 +32,24 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        // Eloquent のすべてのクエリをダンプするイベントを受信する 
+        Event::listen(
+            QueryExecuted::class,
+            function (QueryExecuted $event) {
+                $sql = str_replace(array('%', '?'), array('%%', "'%s'"), $event->sql);
+                $fullSql = vsprintf($sql, $event->bindings);
+
+                Log::debug($fullSql);
+
+                /*
+                // クエリだけ別にしたい場合
+                file_put_contents(
+                    storage_path() . DIRECTORY_SEPARATOR . 'logs'
+                    . DIRECTORY_SEPARATOR . 'sql_log.sql',
+                    $fullSql . ";\n",
+                    FILE_APPEND
+                );*/
+            }
+        );
     }
 }
